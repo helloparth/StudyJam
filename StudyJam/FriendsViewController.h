49b101a3fc9be43176078c5dcd42839e7959a623
Andy Russell <arussell123@gmail.com>
2014-03-01 20:57:44 -0800

    add custom friends view controller

---
+++ StudyJam/FriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactsViewController.h
+//  StudyJam
+//
+//  Created by Andy Russell on 3/1/14.
+//  Copyright (c) 2014 StudyJam, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendsViewController : UIViewController
+
+@end
---
+++ StudyJam/FriendsViewController.m
@@ -0,0 +1,39 @@
+//
+//  ContactsViewController.m
+//  StudyJam
+//
+//  Created by Andy Russell on 3/1/14.
+//  Copyright (c) 2014 StudyJam, Inc. All rights reserved.
+//
+
+#import "FriendsViewController.h"
+
+@interface FriendsViewController ()
+
+@end
+
+@implementation FriendsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    NSLog(@"Friends");
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
