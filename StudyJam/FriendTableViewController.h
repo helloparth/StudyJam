8494b0cbc9ce933066e2828be8c1086b8df2d387
Andy Russell <arussell123@gmail.com>
2014-04-14 01:08:16 -0700

    create FriendTableViewController

---
+++ StudyJam/FriendTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriendTableViewController.h
+//  StudyJam
+//
+//  Created by Andy Russell on 4/14/14.
+//  Copyright (c) 2014 StudyJam, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendTableViewController : UITableViewController<UITableViewDelegate, UITableViewDataSource>
+
+@end
