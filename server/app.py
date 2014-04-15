220524aeb653ba9ec1179ddb734345c4980210b9
Andy Russell <arussell123@gmail.com>
2014-04-14 22:52:12 -0700

    fix incorrect function name in server

--- server/app.py
+++ server/app.py
@@ -17,7 +17,7 @@ def get_friends(user_id):
 
 @app.route('/api/user/<int:user_id>/location')
 def get_location(user_id):
-    return json.dumps(db_interface.get_user_location(user_id))
+    return json.dumps(db_interface.get_location(user_id))
 
 if __name__ == '__main__':
     app.run(debug=True)
