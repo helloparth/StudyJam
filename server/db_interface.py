5a49a240b7d75d43964edff7bae2cd336d23d862
Andy Russell <arussell123@gmail.com>
2014-04-29 11:13:57 -0700

    connect to MongoDB

--- server/db_interface.py
+++ server/db_interface.py
@@ -1,50 +1,28 @@
+from pymongo import MongoClient
+client = MongoClient('mongodb://study:jam@ds031328.mongolab.com:31328/study-jam')
+db = client['study-jam']
+users = db.users
+friends = db.friends
+locations = db.locations
+
 """Abstracts interaction with the database into a simple set of get and update
 functions."""
 
 def get_friends(user_id):
     """Returns a dictionary containing a list of a user's friends' user ids."""
-    if user_id == 4045130232:
-        return { 'friends': [4046427433, 1233549394] }
-    return None
+    return friends.find_one({"_id": user_id})
 
 
 def get_info(user_id):
     """Returns a dictionary containing a user's profile information."""
-    if user_id == 4045130232:
-        return { 'user': {
-                    'first_name': 'Parth',
-                    'last_name': 'Patel'
-                 }
-        }
-    return None
+    return users.find_one({"_id": user_id})
 
 
 def get_location(user_id):
     """Returns a dictionary containing a user's latitude, longitude, and study
     level."""
-    if user_id == 4045130232:
-        return { 'locations': {
-                    'latitude': 34.1000167,
-                    'longitude': -117.7131484,
-                    'study_level': 5
-                 }
-        }
-    elif user_id == 4046427433:
-        return { 'locations': {
-                    'latitude': 34.102108,
-                    'longitude': -117.712601,
-                    'study_level': 3,
-                 }
-        }
-    elif user_id == 1233549394:
-        return { 'locations': {
-                    'latitude': 34.094578,
-                    'longitude': -117.714612,
-                    'study_level': 1,
-                 }
-        }
-    return None
+    return locations.find_one({"_id": user_id})
 
 
 def update_location(user_id, latitude, longitude):
-    pass
+    locations.update({"_id": user_id, "latitude": latitude, "longitude": longitude})
